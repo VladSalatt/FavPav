f16995cf0529b973424c4662a5bc995e6ff67935
Vladislav Koshelev <v.salantis@gmail.com>
2024-01-18 13:44:12 +0300

    delete tuist initial project

--- Targets/FavPevKit/Tests/FavPevKitTests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class FavPevKitTests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("FavPevKit", "FavPevKit")
-    }
-}
--- Targets/FavPevUI/Tests/FavPevUITests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class FavPevUITests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("FavPevUI", "FavPevUI")
-    }
-}
